28a7af9e800a88b29e483ec15dedc1153161d8a9
jebonzla <134077568+jebonzla@users.noreply.github.com>
2024-11-12 19:40:49 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -13,18 +13,18 @@ pip3 install Flask[async]
     touch "/root/iptv-panel/banned/banned_userid.txt"
     touch "/root/iptv-panel/banned/banned_ip.txt"
 }
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/templates/reseller_users.html" >/root/iptv-panel/templates/reseller_users.html
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/menu.sh" >/usr/bin/menu && chmod +x /usr/bin/menu
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/start_bot.sh" >/usr/bin/start_bot.sh && chmod +x /usr/bin/start_bot.sh
-#curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/ott_sam.sh" >/usr/bin/ott_sam.sh && chmod +x /usr/bin/ott_sam.sh
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/main.py" >/root/iptv-panel/main.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/system_ott.py" >/root/iptv-panel/system_ott.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/reseller.py" >/root/iptv-panel/reseller.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/vod.py" >/root/iptv-panel/vod.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/sam_enc.py" >/root/iptv-panel/sam_enc.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/sam_secure.py" >/root/iptv-panel/sam_secure.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/__init__.py" >/root/iptv-panel/pytransform/__init__.py
-curl -s "https://raw.githubusercontent.com/syfqsamvpn/iptv-panel/main/pytransform/_pytransform.so" >/root/iptv-panel/pytransform/_pytransform.so
+curl -s "https://raw.githubusercontent.com/jebonzla/iptv-panel/main/templates/reseller_users.html" >/root/iptv-panel/templates/reseller_users.html
+curl -s "https://raw.githubusercontent.com/jebonzla/iptv-panel/main/menu.sh" >/usr/bin/menu && chmod +x /usr/bin/menu
+curl -s "https://raw.githubusercontent.com/jebonzla/iptv-panel/main/start_bot.sh" >/usr/bin/start_bot.sh && chmod +x /usr/bin/start_bot.sh
+#curl -s "https://raw.githubusercontent.com/jebonzla/iptv-panel/main/ott_sam.sh" >/usr/bin/ott_sam.sh && chmod +x /usr/bin/ott_sam.sh
+curl -s "https://raw.githubusercontent.com/jebonzla/iptv-panel/main/main.py" >/root/iptv-panel/main.py
+curl -s "https://raw.githubusercontent.com/jebonzla/iptv-panel/main/system_ott.py" >/root/iptv-panel/system_ott.py
+curl -s "https://raw.githubusercontent.com/jebonzla/iptv-panel/main/reseller.py" >/root/iptv-panel/reseller.py
+curl -s "https://raw.githubusercontent.com/jebonzla/iptv-panel/main/vod.py" >/root/iptv-panel/vod.py
+curl -s "https://raw.githubusercontent.com/jebonzla/iptv-panel/main/sam_enc.py" >/root/iptv-panel/sam_enc.py
+curl -s "https://raw.githubusercontent.com/jebonzla/iptv-panel/main/sam_secure.py" >/root/iptv-panel/sam_secure.py
+curl -s "https://raw.githubusercontent.com/jebonzla/iptv-panel/main/pytransform/__init__.py" >/root/iptv-panel/pytransform/__init__.py
+curl -s "https://raw.githubusercontent.com/jebonzla/iptv-panel/main/pytransform/_pytransform.so" >/root/iptv-panel/pytransform/_pytransform.so
 
 if [ "$(grep -wc "REFF_STAT" "/root/iptv-panel/data.txt")" == '0' ]; then
     echo 'REFF_STAT = "on"                                                                        # Should be on/off (case sensitive)' >>"/root/iptv-panel/data.txt"
